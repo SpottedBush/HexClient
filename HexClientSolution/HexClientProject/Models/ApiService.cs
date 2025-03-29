0ecfa3b8c80502eeac3f2f94e9c6aa2747621263
Vincent <vincent.tardieux@pravaig.com>
2025-03-29 14:12:29 +0530

    fix: fixing the projects linking

--- HexClientSolution/HexClientProject/Models/ApiService.cs
+++ HexClientSolution/HexClientProject/Models/ApiService.cs
@@ -8,14 +8,14 @@ namespace HexClientProject.Models
     {
         public static async void CreateLobby(string gameId)
         {
-            LcuApi.ILeagueClient api = await LcuApi.LeagueClient.Connect();
+            ILeagueClient api = await LeagueClient.Connect();
             var body = new { queueId = gameId };
             System.Net.Http.HttpResponseMessage response = await api.MakeApiRequest(HttpMethod.Post, "lol-lobby/v2/lobby/", body);
             String responseStr = await response.Content.ReadAsStringAsync();
             
             if (!response.IsSuccessStatusCode)
             {
-                throw new Exception("Err: Cannot create the lobby " + GameMode.GetGameModeFromGameId(gameId) + " - Return code: " + response.StatusCode.ToString());
+                throw new Exception("Err: Cannot create the lobby " + GameMode.GetGameModeFromGameId(gameId) + " - Return code: " + response.StatusCode);
             }
         }
 
